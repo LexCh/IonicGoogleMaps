52fc0c10b453fd1d97fad0c231688e866f20b25c
LexCh <alex.chsql@gmail.com>
2020-05-28 23:21:31 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
+
+declare var google: any;
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,99 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  map: any;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
+  infoWindows: any = [];
+  markers: any = [
+    {
+        title: "UDLA Granados",
+        latitude: "-0.1681142",
+        longitude: "-78.4725959"
+    },
+    {
+      title: "UDLA Queri",
+      latitude: "-0.1690388",
+      longitude: "-78.4707835"
+    },
+    {
+        title: "UDLA Park",
+        latitude: "-0.1631871",
+        longitude: "-78.4592427"
+    },
+    {
+        title: "Parque Metropolitano",
+        latitude: "-0.1775428",
+        longitude: "-78.4581755"
+    },
+    {
+          title: "Sanduches del Arbolito",
+          latitude: "-0.1569785",
+          longitude: "-78.4663163"
+      }
+  ]
   constructor() {}
 
+  ionViewDidEnter() {
+    this.showMap();
+  }
+  
+  addMarkersToMap(markers) {
+    for (let marker of markers) {
+      let position = new google.maps.LatLng(marker.latitude, marker.longitude);
+      let mapMarker = new google.maps.Marker({
+        position: position,
+        title: marker.title,
+        latitude: marker.latitude,
+        longitude: marker.longitude
+      });
+
+      mapMarker.setMap(this.map);
+      this.addInfoWindowToMarker(mapMarker);
+    }
+  }
+
+  addInfoWindowToMarker(marker) {
+    let infoWindowContent = '<div id="content">' +
+                              '<h2 id="firstHeading" class"firstHeading">' + marker.title + '</h2>' +
+                              '<p>Latitude: ' + marker.latitude + '</p>' +
+                              '<p>Longitude: ' + marker.longitude + '</p>' +
+                              '<ion-button id="navigate">Navigate</ion-button>' +
+                            '</div>';
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: infoWindowContent
+    });
+
+    marker.addListener('click', () => {
+      this.closeAllInfoWindows();
+      infoWindow.open(this.map, marker);
+      google.maps.event.addListenerOnce(infoWindow, 'domready', () => {
+        document.getElementById('navigate').addEventListener('click', () => {
+          console.log('navigate button clicked!');
+          // Código para navegar usando la aplicación google maps
+          window.open('https://www.google.com/maps/dir/?api=1&destination=' + marker.latitude + ',' + marker.longitude);
+        });
+      });
+    });
+    this.infoWindows.push(infoWindow);
+  }
+
+  closeAllInfoWindows() {
+    for(let window of this.infoWindows) {
+      window.close();
+    }
+  }
+
+  showMap() {
+    const location = new google.maps.LatLng(-0.1636523,-78.4663138);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    this.addMarkersToMap(this.markers);
+  }
 }
